183c301a4a7c61e2c75beb512aa52def6e143b22
ainwg <87463461+ainwg@users.noreply.github.com>
2023-09-01 13:06:34 +0800

    Final commit

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoAdapter: TodoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        val rvToDoItems: RecyclerView = findViewById(R.id.rvToDoItems)
+        val btnAddItem: Button = findViewById(R.id.btnAddItem)
+        val etToDoTitle: EditText = findViewById(R.id.etToDoTitle)
+        val btnDelete: Button = findViewById(R.id.btnDelete)
+
+        rvToDoItems.adapter = todoAdapter
+        rvToDoItems.layoutManager = LinearLayoutManager(this)
+
+        btnAddItem.setOnClickListener {
+            val todoTitle = etToDoTitle.text.toString()
+            if(todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etToDoTitle.text.clear()
+            }
+        }
+
+        btnDelete.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
     }
 }
--- app/src/main/java/com/example/todolist/TodoAdapter.kt
+++ app/src/main/java/com/example/todolist/TodoAdapter.kt
@@ -1,7 +1,11 @@
 package com.example.todolist
 
+import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class TodoAdapter (
@@ -10,14 +14,54 @@ class TodoAdapter (
     class TodoViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
-        TODO("Not yet implemented")
+        return TodoViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_todo,
+                parent,
+                false
+            )
+        )
+    }
+
+    fun addTodo (todo: Todo) {
+        todos.add(todo)
+        notifyItemInserted(todos.size - 1)
+    }
+
+    fun deleteDoneTodos () {
+        todos.removeAll {todo ->
+            todo.isChecked
+        }
+        notifyDataSetChanged()
+    }
+
+    private fun toggleStrikeThrough (tvTodoList: TextView, isChecked: Boolean) {
+        if(isChecked) {
+            tvTodoList.paintFlags = tvTodoList.paintFlags or STRIKE_THRU_TEXT_FLAG
+        }
+        else {
+            tvTodoList.paintFlags = tvTodoList.paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
+        }
     }
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val curtodo = todos[position]
+        holder.itemView.apply {
+            val tvTodoList = findViewById<TextView>(R.id.tvTodoList) // Replace with the actual ID
+            val cbDone = findViewById<CheckBox>(R.id.cbDone) // Replace with the actual ID
+
+            tvTodoList.text = curtodo.title
+            cbDone.isChecked = curtodo.isChecked
+
+            toggleStrikeThrough(tvTodoList, cbDone.isChecked)
+            cbDone.setOnCheckedChangeListener { _, isChecked ->
+                toggleStrikeThrough(tvTodoList, isChecked)
+                curtodo.isChecked = !curtodo.isChecked
+            }
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return todos.size
     }
 }
